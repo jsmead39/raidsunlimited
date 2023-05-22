450bdf2f2fc6aca3e62a19fe8f3df97964d62e60
James Smead <jsmead39@yahoo.com>
2023-05-22 12:50:12 -0500

    Created a raidDAO and implemented the save functionality

--- app/src/main/java/raidsunlimited/dynamodb/RaidDao.java
+++ app/src/main/java/raidsunlimited/dynamodb/RaidDao.java
@@ -1,14 +1,33 @@
 package raidsunlimited.dynamodb;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import raidsunlimited.dynamodb.models.RaidEvent;
+import raidsunlimited.metrics.MetricsPublisher;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 /**
- * Accesses data for a raidevent using {@Link RaidEvent} to represent the model in DynamoDB.
+ * Accesses data for a raidevent using {@link raidsunlimited.dynamodb.models.RaidEvent} to represent the model in DynamoDB.
  */
 @Singleton
 public class RaidDao {
     private final DynamoDBMapper dynamoDBMapper;
     private final MetricsPublisher metricsPublisher;
+
+    /**
+     *
+     * @param dynamoDBMapper the {@link DynamoDBMapper} used to interact with the raidevents table
+     * @param metricsPublisher the {@link MetricsPublisher} used to record metrics.
+     */
+    @Inject
+    public RaidDao(DynamoDBMapper dynamoDBMapper, MetricsPublisher metricsPublisher) {
+        this.dynamoDBMapper = dynamoDBMapper;
+        this.metricsPublisher = metricsPublisher;
+    }
+
+    public RaidEvent saveRaid(RaidEvent raidEvent) {
+        this.dynamoDBMapper.save(raidEvent);
+        return raidEvent;
+    }
 }
