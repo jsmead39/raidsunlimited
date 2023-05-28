8d22f6b8a22bafb5ba93d06cf123f4986f10d190
James Smead <jsmead39@yahoo.com>
2023-05-28 09:19:00 -0500

    Added a Result class for our Profile Creator, added a GameCharacterToProfile for future use

--- app/src/main/java/raidsunlimited/activity/results/CreateProfileResult.java
+++ app/src/main/java/raidsunlimited/activity/results/CreateProfileResult.java
@@ -2,8 +2,95 @@ package raidsunlimited.activity.results;
 
 import raidsunlimited.models.GameCharacter;
 
-public class CreateProfileResult {
-    private final GameCharacter gameCharacter;
+import java.util.ArrayList;
+import java.util.List;
 
-    private CreateProfile
+public class CreateProfileResult {
+    private final String userId;
+    private final String displayName;
+    private final String email;
+    private final List<GameCharacter> charactersList;
+    private final String logs;
+
+    private CreateProfileResult(String userId, String displayName, String email, List<GameCharacter> charactersList,
+                          String logs) {
+        this.userId = userId;
+        this.displayName = displayName;
+        this.email = email;
+        this.charactersList = charactersList;
+        this.logs = logs;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<GameCharacter> getCharactersList() {
+        return new ArrayList<>(charactersList);
+    }
+
+    public String getLogs() {
+        return logs;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateProfileResult{" +
+                "userId='" + userId + '\'' +
+                ", displayName='" + displayName + '\'' +
+                ", email='" + email + '\'' +
+                ", charactersList=" + charactersList +
+                ", logs='" + logs + '\'' +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static CreateRaidResult.Builder builder() {
+        return new CreateRaidResult.Builder();
+    }
+
+    public static class Builder {
+        private String userId;
+        private String displayName;
+        private String email;
+        private List<GameCharacter> charactersList;
+        private String logs;
+
+        public Builder withUserId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder withDisplayName(String displayName) {
+            this.displayName = displayName;
+            return this;
+        }
+
+        public Builder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder withCharacterList(List<GameCharacter> charactersList) {
+            this.charactersList = new ArrayList<>(charactersList);
+            return this;
+        }
+
+        public Builder withLogs(String logs) {
+            this.logs = logs;
+            return this;
+        }
+
+        public CreateProfileResult build() {
+            return new CreateProfileResult(userId, displayName, email, charactersList, logs);
+        }
+    }
 }
