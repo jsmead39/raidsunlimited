750bd47cf54d03801f96f7549d4209d3c3cac5c4
James Smead <jsmead39@yahoo.com>
2023-06-06 10:54:07 -0500

    Added userRaidDao, this will allow for saving a user_raid for a signed up event

---
+++ app/src/main/java/raidsunlimited/dynamodb/UserRaidDao.java
@@ -0,0 +1,43 @@
+package raidsunlimited.dynamodb;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import raidsunlimited.dynamodb.models.RaidEvent;
+import raidsunlimited.dynamodb.models.UserRaid;
+import raidsunlimited.exceptions.RaidEventNotFoundException;
+import raidsunlimited.metrics.MetricsConstants;
+import raidsunlimited.metrics.MetricsPublisher;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Accesses the user_Raid table to store information on what raids a user has signed up for and if they are confirmed.
+ */
+@Singleton
+public class UserRaidDao {
+    private final DynamoDBMapper dynamoDBMapper;
+    private final MetricsPublisher metricsPublisher;
+
+    /**
+     *
+     * @param dynamoDBMapper the {@link DynamoDBMapper} used to interact with the raidevents table
+     * @param metricsPublisher the {@link MetricsPublisher} used to record metrics.
+     */
+    @Inject
+    public UserRaidDao(DynamoDBMapper dynamoDBMapper, MetricsPublisher metricsPublisher) {
+        this.dynamoDBMapper = dynamoDBMapper;
+        this.metricsPublisher = metricsPublisher;
+    }
+
+    /**
+     * Add or updates a record in the user_raid table
+     * @param userRaid The userRaid objects representing the record to be added.
+     * @return
+     */
+    public UserRaid saveToEvent(UserRaid userRaid) {
+        this.dynamoDBMapper.save(userRaid);
+        return userRaid;
+    }
+
+
+}
