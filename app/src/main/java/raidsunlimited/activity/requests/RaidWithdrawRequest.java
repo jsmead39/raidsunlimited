0d0bd9fdd505279806c2a117fae116847ee99cad
James Smead <jsmead39@yahoo.com>
2023-06-14 22:06:48 -0500

    Added request class for endpoint

---
+++ app/src/main/java/raidsunlimited/activity/requests/RaidWithdrawRequest.java
@@ -0,0 +1,56 @@
+package raidsunlimited.activity.requests;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+@JsonDeserialize(builder = RaidWithdrawRequest.Builder.class)
+public class RaidWithdrawRequest {
+    private final String raidId;
+    private final String userId;
+
+    private RaidWithdrawRequest(String raidId, String userId) {
+        this.raidId = raidId;
+        this.userId = userId;
+    }
+
+    public String getRaidId() {
+        return raidId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "RaidWithdrawRequest{" +
+                "raidId='" + raidId + '\'' +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @JsonPOJOBuilder
+    public static class Builder {
+        private String raidId;
+        private String userId;
+
+        public Builder withRaid(String raidId) {
+            this.raidId = raidId;
+            return this;
+        }
+
+        public Builder withUserId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public RaidWithdrawRequest build() {
+            return new RaidWithdrawRequest(raidId, userId);
+        }
+    }
+}
